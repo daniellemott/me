350596affcd3661a403ee6b4c7929b94bc90cd59
Danielle Mott <danielle.mott123@gmail.com>
2020-04-29 15:14:41 -0700

    Cleanup code

--- src/views/Components/Gallery/Gallery.js
+++ src/views/Components/Gallery/Gallery.js
@@ -2,19 +2,19 @@ import React from "react";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 
-import studio1 from "assets/img/ProfilePage/Gnus.JPG";
-import studio2 from "assets/img/ProfilePage/Mountains.jpg";
-import studio3 from "assets/img/ProfilePage/Skiing.JPG";
-import studio4 from "assets/img/ProfilePage/XCSkiing.jpg";
-import studio5 from "assets/img/ProfilePage/Sunset.jpg";
-import studio6 from "assets/img/ProfilePage/Nellie.jpg";
-import studio7 from "assets/img/ProfilePage/waterfall.jpg";
-import studio8 from "assets/img/ProfilePage/portugal.jpg";
-import studio9 from "assets/img/ProfilePage/hummus.JPG";
-import studio10 from "assets/img/ProfilePage/sushi.jpg";
-import studio11 from "assets/img/ProfilePage/mountains_hat.jpg";
-import studio12 from "assets/img/ProfilePage/jump.jpg";
-import studio13 from "assets/img/ProfilePage/kenya.jpg";
+import Gnus from "assets/img/GalleryImg/Gnus.jpg";
+import Mountains from "assets/img/GalleryImg/Mountains.jpg";
+import Skiing from "assets/img/GalleryImg/Skiing.jpg";
+import XCSkiing from "assets/img/GalleryImg/XCSkiing.jpg";
+import Sunset from "assets/img/GalleryImg/Sunset.jpg";
+import Nellie from "assets/img/GalleryImg/Nellie.jpg";
+import Waterfall from "assets/img/GalleryImg/Waterfall.jpg";
+import Portugal from "assets/img/GalleryImg/Portugal.jpg";
+import Hummus from "assets/img/GalleryImg/Hummus.jpg";
+import Sushi from "assets/img/GalleryImg/Sushi.jpg";
+import Mountains_Hat from "assets/img/GalleryImg/Mountains_Hat.jpg";
+import Jump from "assets/img/GalleryImg/Jump.jpg";
+import Kenya from "assets/img/GalleryImg/Kenya.jpg";
 
 import classNames from "classnames";
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
@@ -22,31 +22,31 @@ import styles from "assets/jss/material-kit-react/views/profilePage.js";
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(styles);
 
-export default function Gallery(props) {
+export default function Gallery() {
   const classes = useStyles();
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
   return (
     <GridContainer justify="center">
       <GridItem xs={12} sm={12} md={3}>
-        <img alt="..." src={studio5} className={navImageClasses} />
-        <img alt="..." src={studio1} className={navImageClasses} />
-        <img alt="..." src={studio11} className={navImageClasses} />
+        <img alt="..." src={Sunset} className={navImageClasses} />
+        <img alt="..." src={Gnus} className={navImageClasses} />
+        <img alt="..." src={Mountains_Hat} className={navImageClasses} />
       </GridItem>
       <GridItem xs={12} sm={12} md={3}>
-        <img alt="..." src={studio2} className={navImageClasses} />
-        <img alt="..." src={studio9} className={navImageClasses} />
-        <img alt="..." src={studio4} className={navImageClasses} />
-        <img alt="..." src={studio12} className={navImageClasses} />
+        <img alt="..." src={Mountains} className={navImageClasses} />
+        <img alt="..." src={Hummus} className={navImageClasses} />
+        <img alt="..." src={XCSkiing} className={navImageClasses} />
+        <img alt="..." src={Jump} className={navImageClasses} />
       </GridItem>
       <GridItem xs={12} sm={12} md={3}>
-        <img alt="..." src={studio8} className={navImageClasses} />
-        <img alt="..." src={studio13} className={navImageClasses} />
-        <img alt="..." src={studio6} className={navImageClasses} />
+        <img alt="..." src={Portugal} className={navImageClasses} />
+        <img alt="..." src={Kenya} className={navImageClasses} />
+        <img alt="..." src={Nellie} className={navImageClasses} />
       </GridItem>
       <GridItem xs={12} sm={12} md={3}>
-        <img alt="..." src={studio3} className={navImageClasses} />
-        <img alt="..." src={studio7} className={navImageClasses} />
-        <img alt="..." src={studio10} className={navImageClasses} />
+        <img alt="..." src={Skiing} className={navImageClasses} />
+        <img alt="..." src={Waterfall} className={navImageClasses} />
+        <img alt="..." src={Sushi} className={navImageClasses} />
       </GridItem>
     </GridContainer>
   );
