a2ff03c73607cc26fb74c039dfcece5ee7eda357
Danielle Mott <danielle.mott123@gmail.com>
2020-06-03 19:24:30 -0700

    Revert to social media links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,20 @@
+
 import React from "react";
 import PropTypes from "prop-types";
+
+import Button from "../CustomButtons/Button.js";
+import { List, ListItem } from "@material-ui/core";
 
 import classNames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/material-kit-react/components/footerStyle.js";
 
-import { QuoteWrapper } from "./styled"
-
 const useStyles = makeStyles(styles);
+const LinksDict = {
+  "mailto:danielle.mott123@gmail.com": "fa fa-envelope",
+  "https://github.com/daniellemott": "fab fa-github",
+  "https://www.facebook.com/danielleelizabethm": "fab fa-facebook"
+};
 
 export default function Footer(props) {
   const classes = useStyles();
@@ -19,10 +26,15 @@ export default function Footer(props) {
   return (
     <footer className={footerClasses}>
       <div className={classes.container}>
-        <QuoteWrapper className={classes.title}>
-          The roots of all goodness lie in the soil of appreciation for goodness. 
-          <div>Dalai Lama</div>
-      </QuoteWrapper>
+        <List className={classes.list}>
+          {Object.keys(LinksDict).map((Link, index) => (
+            <ListItem className={classes.inlineBlock} key={index}>
+              <Button justIcon link className={classes.margin5} href={Link}>
+                <i className={LinksDict[Link]} />
+              </Button>
+            </ListItem>
+          ))}
+        </List>
       </div>
     </footer>
   );
